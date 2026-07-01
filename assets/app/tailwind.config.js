8ad759dd3b6fa5279eaff3a6a1574581544f0a01
Alireza <elyasfgidev@gmail.com>
2026-07-01 13:16:43 +0330

    Add files via upload

---
+++ assets/app/tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    darkMode: 'class', // 👈 Crucial context wrapper for element toggles
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+}
